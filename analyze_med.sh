a8b4e47edf0838ab977767aee6f946fe4a743d2a
Vdain <bradley.euell@gmail.com>
2024-06-23 17:44:58 -0500

    chore: Update media directory path and improve subtitle processing logic

--- analyze_med.sh
+++ analyze_med.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-MEDIA_DIR="/mnt/c/Users/Bradl/OneDrive/CloudVids/NeedsProcessing"
+# MEDIA_DIR="/mnt/c/Users/Bradl/OneDrive/CloudVids/NeedsProcessing"
+MEDIA_DIR="/mnt/seedbox/stash/TestFolder"
 # Output file for the report
 REPORT_FILE="subtitle_report.csv"
-
+echo "File---Streams" >"$REPORT_FILE"
 # whis --output_dir ../SubsOut --output_format srt --task translate --language ja "$input"
-
+TO_PROCESS_FILE="to_process.txt"
+echo -n "" >"$TO_PROCESS_FILE"
 function get_base_name() {
    local filename=$(basename -- "$1")
    echo "${filename%%.*}"
@@ -60,22 +62,24 @@ function process_media_file() {
    ffmpeg -y -ss 00:05:00 -i "$file" -t 00:00:30 -c copy "sample_$base_name" 2>/dev/null
    language=$(whisper --task translate --verbose False --output_format json "sample_$base_name" 2>/dev/null) && rm "sample_$base_name" && rm "sample_${base_name%.*}.json"
    lang=$(echo "${language##*language: }")
-   echo "$file,$lang" >>"$report_file"
+   echo "$file---$lang" >>"$report_file"
 
    if [[ $lang == "English" ]]; then
-      echo "$file,$lang" >>"$report_file"
+      echo "$file---$lang" >>"$report_file"
    else
       foundEng=false
       for stream in $subtitle_info; do
-         echo "$file,$stream" >>"$report_file"
-         if [[ $stream =~ "subtitle;eng" ]]; then
+         echo "$file---$stream" >>"$report_file"
+         if [[ "$stream" =~ "subtitle;eng" ]]; then
             foundEng=true
          fi
       done
       if [[ $foundEng == false ]]; then
          echo "Translating $file to English"
+         # echo "$file" >>"$TO_PROCESS_FILE"
+         dir=${file%/*}
          # whis --output_dir ../SubsOut --output_format srt --task translate --language ja "$input"
-         whisper --model_dir "$WHISPER_CACHE" --verbose False --output_dir "$MEDIA_DIR/SubsOut" --output_format srt --task translate --language ja "$file"
+         whisper --model_dir "$WHISPER_CACHE" --verbose False --output_dir "$dir" --output_format srt --task translate --language ja "$file"
       else
          echo "found eng in embedded subs of $file"
       fi
@@ -120,22 +124,22 @@ function pair_media_list() {
       if [[ -z "$matching_subtitle" ]]; then
          echo "Media: $mediafile" >&2
          echo "No matching external subtitle found for $mediafile" >&2
-         echo "$mediafile,No matching external subtitle found" >>"$REPORT_FILE"
+         echo "$mediafile---No matching external subtitle found" >>"$REPORT_FILE"
          # Run your ffprobe command or other processing logic here
          # ffprobe -v error -select_streams a,s -show_entries stream=index,codec_name,codec_type:stream_tags=language,title -of csv=p=0 "$mediafile"
-         # process_media_file "$mediafile" "$REPORT_FILE"
+         process_media_file "$mediafile" "$REPORT_FILE"
       else
          echo "Media: $mediafile" >&2
          echo "Subtitle: $matching_subtitle" >&2
-         echo "$file,$matching_subtitle" >>"$REPORT_FILE"
+         echo "$mediafile---$matching_subtitle" >>"$REPORT_FILE"
       fi
    done <"$media_list"
 
 }
 
 # process_media_files
-testFile="$MEDIA_DIR/Shusaku 1.mkv"
-echo "Processing media file: $testFile"
-process_media_file "$testFile" "subtitle_report.csv"
-# generate_media_list
-# pair_media_list
+# testFile="$MEDIA_DIR/[rawHentai] Dorei Kaigo (奴隷介護) [Dual Audio]/[rawHentai] Dorei Kaigo - 2.mkv"
+# echo "Processing media file: $testFile"
+# process_media_file "$testFile" "subtitle_report_process.csv"
+generate_media_list
+pair_media_list
